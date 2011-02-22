627b139d6cd31f114859762437ae9853d8a018e3
eby <ryaneby@gmail.com>
2011-02-22 18:33:55 -0500

    added a few more tests of the encoding, functions for hex/char conversion and fleshed out encoding function

--- pairtree.class.php
+++ pairtree.class.php
@@ -1,14 +1,30 @@
 <?php
 
 class Pairtree {
-
+ 
   public function encode($identifier) {
-  
+    $encode_regex = "/[\"*+,<=>?\\^|]|[^\x21-\x7e]/";
+    $escaped_string = preg_replace_callback($encode_regex, 'Pairtree::strtohex', $identifier);
+    $escaped_string = str_replace(array('/',':','.'), array('=','+',','),$escaped_string);
+    return $escaped_string;
   }
   
   public function decode($identifier) {
   
   }
+  
+  private function hextostr($matches) {
+    $s=''; 
+    foreach(explode("\n",trim(chunk_split($x,2))) as $h) $s.=chr(hexdec($h)); 
+    return($s); 
+  } 
+
+  private function strtohex($matches) { 
+    $s='';
+    $x=$matches[0];
+    foreach(str_split($x) as $c) $s.= '^'.sprintf("%02x",ord($c)); 
+    return($s); 
+  } 
 
 }
 
--- tests/all_tests.php
+++ tests/all_tests.php
@@ -12,7 +12,22 @@ class TestOfPairtree extends UnitTestCase {
   
   function test_a() {
     $this->assertEqual(Pairtree::encode('a'),'a');
-    $this->assertRoundtrip('a');
+  }
+  
+  function test_space() {
+    $this->assertEqual(Pairtree::encode('hello world'), 'hello^20world');
+  }
+  
+  function test_slash() {
+     $this->assertEqual(Pairtree::encode('/'),'=');
+  }
+  
+  function test_urn() {
+    $this->assertEqual(Pairtree::encode('http://n2t.info/urn:nbn:se:kb:repos-1'), 'http+==n2t,info=urn+nbn+se+kb+repos-1');
+  }
+  
+  function test_wtf() {
+    $this->assertEqual(Pairtree::encode('what-the-*@?#!^!?'), "what-the-^2a@^3f#!^5e!^3f");
   }
 
 }
